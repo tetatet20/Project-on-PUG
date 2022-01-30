f56c2603ffb3f4299a760568b5f30e23ec429672
tetatet20 <romanseregicev@gmail.com>
2022-01-30 23:03:06 +0300

    реализовал через JS раскрывающийся список гостей

--- pages/scripts/index.js
+++ pages/scripts/index.js
@@ -1,1 +1,8 @@
-alert('леха лох')
+const input = document.querySelector('.guests-input');
+const dropdown = document.querySelector('.dropdown-menu');
+
+input.addEventListener('click', inputClickHandler);
+
+function inputClickHandler() {
+    dropdown.classList.toggle('dropdown-menu-show');
+}
