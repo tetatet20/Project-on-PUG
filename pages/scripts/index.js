803ac260dda74e10e8714f087ad7254a3c4654b1
Scket4 <alexeygolubev921@gmail.com>
2022-02-08 22:24:17 +0300

    fix bugs on index page

--- pages/scripts/index.js
+++ pages/scripts/index.js
@@ -1,76 +1,115 @@
-const input = document.querySelector('.guests-input');
-const dropdown = document.querySelector('.dropdown-menu');
+// Выпадающий список при нажатии на инпут
 
-input.addEventListener('click', inputClickHandler);
+// Чтобы не закрывалось по клику на сам дропдаун - сделал открытие по лейблу. Он как-раз отслеживает клик по инпуту
+const input = document.querySelector('.guests');
 
-function inputClickHandler() {
+input.addEventListener('click', openDropdown);
+
+function openDropdown() {
     dropdown.classList.toggle('dropdown-menu-show');
 }
-//Реализовал выпадающий список при нажатии
 
+// Объявляем всякие кнопки, менюшки инпуты
+const dropdown = document.querySelector('.dropdown-menu');
+const guestsInput = document.querySelector('.guests-input');
+const cleanButton = document.querySelector('.clean');
+const applyButton = document.querySelector('.apply');
+
+// объявляем плюсики и минусики и цифры
 const adultsMinus = document.querySelector('.adults-minus');
 const adultsPlus = document.querySelector('.adults-plus');
 const adultsСounter = document.querySelector('.adults-number');
-
-
-adultsMinus.addEventListener('click', function () {
-    if (parseInt(adultsСounter.innerText) > 0) {
-        adultsСounter.innerText = --adultsСounter.innerText;
-    }
-
-})
-
-adultsPlus.addEventListener('click', function () {
-    adultsСounter.innerText = ++adultsСounter.innerText;
-})
-//Реализовал счетчик взрослых гостей
 
 const childrenMinus = document.querySelector('.children-minus');
 const childrenPlus = document.querySelector('.children-plus');
 const childrenСounter = document.querySelector('.children-number');
 
-childrenMinus.addEventListener('click', function () {
-    if (parseInt(childrenСounter.innerText) > 0) {
-        childrenСounter.innerText = --childrenСounter.innerText;
-    }
-})
-
-childrenPlus.addEventListener('click', function () {
-    childrenСounter.innerText = ++childrenСounter.innerText;
-})
-//Реализовал счетчик гостей - детей
-
 const babiesMinus = document.querySelector('.babies-minus');
 const babiesPlus = document.querySelector('.babies-plus');
 const babiesСounter = document.querySelector('.babies-number');
 
-babiesMinus.addEventListener('click', function () {
-    if (parseInt(babiesСounter.innerText) > 0) {
-        babiesСounter.innerText = --babiesСounter.innerText;
+// Создаем счетчики для цифр, чтобы не залезать каждый раз в dom дерево (Так быстрее работает и так правильно)
+let adults = 0;
+let childrens = 0;
+let babies = 0;
+let completeCount = 0;
+
+
+// Методы для увеличения/уменьшания значений. Увеличиваем каунт + вызываем функцию для обнавления текущего значения
+// То есть при каждом уменьшении - увелечении у нас вызывается функция, которая обновляет счетчик и локальный и общий
+adultsMinus.addEventListener('click', () => {
+    if (adults > 0) adults--;
+
+    updateCount(adultsСounter, adults)
+})
+
+adultsPlus.addEventListener('click', () => {
+    adults++
+
+    updateCount(adultsСounter, adults)
+})
+
+childrenMinus.addEventListener('click', () => {
+    if (childrens > 0) {
+        childrens--
     }
+
+    updateCount(childrenСounter, childrens)
+    
 })
 
-babiesPlus.addEventListener('click', function () {
-    babiesСounter.innerText = ++babiesСounter.innerText;
+childrenPlus.addEventListener('click', () => {
+    childrens++
+
+    updateCount(childrenСounter, childrens)
 })
-//Реализовал счетчик гостей - младенцев
 
-const clean = document.querySelector('.clean');
+babiesMinus.addEventListener('click', ()=> {
+    if (babies > 0) {
+        babies--
+    }
 
-clean.addEventListener('click', function () {
-    adultsСounter.innerText = 0
-    childrenСounter.innerText = 0
-    babiesСounter.innerText = 0
-    guestsInput.value = 0 + ' гостей'
+    updateCount(babiesСounter, babies)
 })
-//реализовал сброс счетчиков
 
-const count = document.querySelector('.round-button');
-count.addEventListener('click', inputClickHandler);
-const guestsInput = document.querySelector('.guests-input');
+babiesPlus.addEventListener('click', () => {
+    babies++
 
-function inputClickHandler() {
-    let guestsSum = parseInt(adultsСounter.innerText) + parseInt(childrenСounter.innerText) + parseInt(babiesСounter.innerText)
+    updateCount(babiesСounter, babies)
+})
+
+
+// Удаляем класс для скрытия дропдауна. Тут нет ключевого слова function. Это стрелочная функция, ты должнен про нее прочитать в учебнике
+applyButton.addEventListener('click', () => dropdown.classList.remove('.dropdown-menu-show'))
+
+// Тут чистим все переменные и вызываем методы для обновления локальных счетчиков. Эти же методы обновляют общий счетчик
+// e.stopPropagation() нужна для того, чтобы при клике не закрывалось
+cleanButton.addEventListener('click', (e) => {
+    e.stopPropagation();
+
+    adults = childrens = babies = 0;
+
+    updateCount(adultsСounter, adults);
+    updateCount(childrenСounter, childrens);
+    updateCount(babiesСounter, babies);
+})
+
+
+// Хелпер-функция updateCount
+// Аргументы: countElement, value
+// countElement: сюда передаем элемент count для каждого (взрослый, ребенок тд), который хотим обновить
+// value: сюда передаем значение, которое подставим в countElement
+// Что делает: задает элементу значение, вызывает функцию для обновления общего счетчика
+function updateCount(countElement, value) {
+    countElement.innerText = value;
+    updateCompleteCount();
+}
+
+// Функция для лбновления общего счетчика. складываем переменные (без innerText, потому что это большая нагрузка)
+// Проверяем и подставляем в инпут
+function updateCompleteCount() {
+    const guestsSum = adults + childrens + babies;
+
     if (guestsSum == 1) {
         guestsInput.value = guestsSum + ' гость';
     } else if ((guestsSum > 1) & (guestsSum < 5)) {
@@ -79,4 +118,109 @@ function inputClickHandler() {
         guestsInput.value = guestsSum + ' гостей';
     }
 }
-//реализовал суммирование количества гостей
+
+
+
+
+// ТУТ ДРУГАЯ ВЕРСИЯ ЧЕРЕЗ DATA АТРИБУТЫ. МОЖЕШЬ ЗАПУСТИТЬ, НЕ УДАЛЯЙ, ПОСМОТРИ
+
+// const label = document.querySelector('.guests');
+// const dropdown = document.querySelector('.dropdown-menu');
+// const guestsInput = document.querySelector('.guests-input');
+// const adultsСounter = document.querySelector('.adults-number');
+// const childrenСounter = document.querySelector('.children-number');
+// const babiesСounter = document.querySelector('.babies-number');
+// const applyButton = document.querySelector('.apply');
+// const cleanButton = document.querySelector('.clean');
+
+// let adults = 0;
+// let childrens = 0;
+// let babies = 0;
+// let completeCount = 0;
+
+// label.addEventListener('click', (e) => {
+//     if (e.target.classList[0] === 'selection') return
+
+//     dropdown.classList.toggle('dropdown-menu-show')
+
+// });
+// applyButton.addEventListener('click', () => dropdown.classList.remove('.dropdown-menu-show'))
+// cleanButton.addEventListener('click', (e) => {
+//     e.stopPropagation();
+
+//     adults = childrens = babies = 0;
+
+//     adultsСounter.innerText = 0;
+//     childrenСounter.innerText = 0;
+//     babiesСounter.innerText = 0;
+
+//     updateCompleteCount();
+// })
+
+// dropdown.addEventListener('click', (e) => {
+//     if (!e.target.dataset?.button) return 
+
+//     switch (e.target.dataset.button) {
+//         case 'adult-minus':
+//             if (adults === 0) return
+//             adults--
+//             break;
+//         case 'adult-plus':
+//             adults++
+//             break;
+//         case 'childrens-minus':
+//             if (childrens === 0) return
+//             childrens--
+//             break;
+//         case 'childrens-plus':
+//             childrens++
+//             break;
+//         case 'babies-minus':
+//             if (babies === 0) return
+//             babies--
+//             break;
+//         case 'babies-plus':
+//             babies++
+//             break;
+//     }
+
+//     updateCounts(e.target.dataset.button)
+// })
+
+// function updateCounts(count) {
+//     switch (count) {
+//         case 'adult-minus':
+//         case 'adult-plus':
+//             adultsСounter.innerText = adults;
+//             break;
+
+//         case 'childrens-minus':
+//         case 'childrens-plus':
+//             childrenСounter.innerText = childrens;
+//             break;
+    
+//         case 'babies-minus':
+//         case 'babies-plus':
+//             babiesСounter.innerText = babies;
+//             break;
+//     }
+
+//     updateCompleteCount();
+// }
+
+
+// function updateCompleteCount() {
+//     const guestsSum = adults + childrens + babies;
+
+//     if (guestsSum == 1) {
+//         guestsInput.value = guestsSum + ' гость';
+//     } else if ((guestsSum > 1) & (guestsSum < 5)) {
+//         guestsInput.value = guestsSum + ' гостя';
+//     } else if (guestsSum == 0) {
+//         guestsInput.value = null;
+//         guestsInput.placeholder = 'Сколько гостей';
+//     } else {
+//         guestsInput.value = guestsSum + ' гостей';
+//     }
+// }
+
