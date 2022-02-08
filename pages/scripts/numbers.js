424ea75b548d637eb32a205eb8ee8ed8a49145f7
tetatet20 <romanseregicev@gmail.com>
2022-02-08 23:07:57 +0300

    Начал верстать панель фильтров

---
+++ pages/scripts/numbers.js
@@ -0,0 +1,34 @@
+const checkbox_1= document.querySelector('#checkbox_1');
+checkbox_1.addEventListener('click', clickHandler_1);
+
+function clickHandler_1() {
+checkbox_1.classList.toggle('checkbox-active');
+}
+
+const checkbox_2= document.querySelector('#checkbox_2');
+checkbox_2.addEventListener('click', clickHandler_2);
+
+function clickHandler_2() {
+checkbox_2.classList.toggle('checkbox-active');
+}
+
+const checkbox_3= document.querySelector('#checkbox_3');
+checkbox_3.addEventListener('click', clickHandler_3);
+
+function clickHandler_3() {
+checkbox_3.classList.toggle('checkbox-active');
+}
+
+const checkbox_4= document.querySelector('#checkbox_4');
+checkbox_4.addEventListener('click', clickHandler_4);
+
+function clickHandler_4() {
+checkbox_4.classList.toggle('checkbox-active');
+}
+
+const checkbox_5= document.querySelector('#checkbox_5');
+checkbox_5.addEventListener('click', clickHandler_5);
+
+function clickHandler_5() {
+checkbox_5.classList.toggle('checkbox-active');
+}
