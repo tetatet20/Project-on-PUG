d65844238329ef60f24956ca45ae24b2d71f7322
tetatet20 <romanseregicev@gmail.com>
2022-05-14 22:23:50 +0300

    продолжаю делать адаптив главной страницы

--- src/main.js
+++ src/main.js
@@ -34,11 +34,34 @@ link.forEach(function (link) {
 });
 
 //Код для меню в подвале
-// const linkPassive = document.querySelectorAll(".color-monserat");
-// const boldLink = document.querySelectorAll(".bold-nav-footer");
 
-// boldLink.forEach(function (linkes) {
-//   linkes.addEventListener("click", function () {
-//     linkPassive.classList.remove("color-monserat-active");
-//   });
-// });
+const menuLinkFirst = document.querySelector(".navigation-footer");
+const menuLinkSecond = document.querySelector(".about-us");
+const menuLinkThird = document.querySelector(".support-service");
+const linkPassiveFirst = document.querySelectorAll("#first");
+const linkPassiveSecond = document.querySelectorAll("#second");
+const linkPassiveThird = document.querySelectorAll("#third");
+const menuLinkFourth = document.querySelector(".bold-nav-footer-subscription");
+const linkPassiveFourth = document.querySelector(".subscription-email");
+
+linkPassiveFirst.forEach(function (second) {
+  menuLinkFirst.addEventListener("click", function () {
+    second.classList.toggle("link-active");
+  });
+});
+
+linkPassiveSecond.forEach(function (first) {
+  menuLinkSecond.addEventListener("click", function () {
+    first.classList.toggle("link-active");
+  });
+});
+
+linkPassiveThird.forEach(function (third) {
+  menuLinkThird.addEventListener("click", function () {
+    third.classList.toggle("link-active");
+  });
+});
+
+menuLinkFourth.addEventListener("click", function () {
+  linkPassiveFourth.classList.toggle("subscription-margin-active");
+});
