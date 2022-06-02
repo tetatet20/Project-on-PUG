6c59edfaaa26d716cf7799b7d4c368c47854969a
tetatet20 <romanseregicev@gmail.com>
2022-06-02 11:49:59 +0300

    сделал адаптив для регистрации и авторизации

--- docs/be5e057c651fcb45a146.js
+++
@@ -1,1 +0,0 @@
-const containerRadio=document.querySelector(".container-radio"),radio=document.querySelector(".radio");containerRadio.addEventListener("click",(e=>{"selection"!==e.target.classList[0]&&radio.classList.toggle("radio-active")}));
